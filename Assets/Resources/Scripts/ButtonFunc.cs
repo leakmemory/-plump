44b58a5d1f563695dde8fec702c767963f7113c0
leakmemory <vinokurovdmitryy@yandex.ru>
2018-09-05 22:20:37 +0300

    Затемнение при старте новой игры

--- Assets/Resources/Scripts/ButtonFunc.cs
+++ Assets/Resources/Scripts/ButtonFunc.cs
@@ -2,11 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonFunc : MonoBehaviour {
 
+	[SerializeField]
+	private GameObject darkScreen;
+	[SerializeField]
+	private GameObject playAgainBtn;
+
+	private SpriteRenderer darkScreenSprite;
+	private Image playAgainImg; // картинка
+	private Text playAgainTxt; // и текст кнопки
+	private float deltaDisappear = 0f;
+
+	void Start() {
+		darkScreenSprite = darkScreen.GetComponent<SpriteRenderer>();
+		playAgainImg = playAgainBtn.GetComponent<Image>();
+		playAgainTxt = playAgainImg.GetComponentInChildren<Text>();
+	}
+
 	public void StartNewGame() {
-		SceneManager.LoadScene("Game");
+		deltaDisappear = 2f;
+	}
+
+	void Update() {
+		if (deltaDisappear > 0) {
+			if (darkScreenSprite.color.a >= 1f) {
+				SceneManager.LoadScene("Game");
+			}
+			
+			// появление черного экрана
+			Color color = darkScreenSprite.color;
+			color.a += deltaDisappear * Time.deltaTime;
+			darkScreenSprite.color = color;
+
+			// исчезновение кнопки
+			color = playAgainImg.color;
+			color.a -= 2 * deltaDisappear * Time.deltaTime;
+			playAgainImg.color = color;
+			playAgainTxt.color = color;
+
+		}
 	}
 
 }
--- Assets/Resources/Scripts/Camera.cs
+++ Assets/Resources/Scripts/Camera.cs
@@ -8,11 +8,28 @@ public class Camera : MonoBehaviour {
 	private Transform target; // игрок
 	[SerializeField]
 	private GameObject loseWidget;
+	[SerializeField]
+	private GameObject darkScreen;
+
+	private float deltaDark = 2f;
+
+	private SpriteRenderer darkScreenSprite;
+
+	void Start() {
+		darkScreenSprite = darkScreen.GetComponent<SpriteRenderer>();
+	}
 
 	private bool lose = false; 
 	private float time = 1f; // время падения камеры при проигрыше
 	// Update is called once per frame
 	void Update () {
+		// рассеивание черного экрана перед стартом игры
+		if (darkScreenSprite.color.a > 0 && !loseWidget.activeSelf) {
+			Color color = darkScreenSprite.color;
+			color.a -= deltaDark * Time.deltaTime;
+			darkScreenSprite.color = color;
+		}
+
 		if (lose && time > 0) {
 			transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, target.position.y, transform.position.z), Time.deltaTime * 70);
 			time -= Time.deltaTime;
