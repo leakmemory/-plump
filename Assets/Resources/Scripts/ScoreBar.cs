32cac9db14fffdb342a26d5597bc830913c0673a
leakmemory <vinokurovdmitryy@yandex.ru>
2018-09-10 16:08:08 +0300

    Добавил ScoreBar

---
+++ Assets/Resources/Scripts/ScoreBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBar : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject player;
+
+	private Text score;
+	private int maxScore = 0;
+
+	// Use this for initialization
+	void Start () {
+		score = GetComponentInChildren<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.transform.position.y * 10 > maxScore) {
+			maxScore = (int)(player.transform.position.y * 10);
+			score.text = maxScore.ToString();
+		}
+	}
+}
